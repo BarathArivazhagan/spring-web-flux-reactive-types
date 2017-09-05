6ce43a3ecdac850efaacd8dc0a1314a29cae1bbc
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-09-05 13:44:24 +0530

    code commit

---
+++ src/main/java/com/barath/app/FluxUserController.java
@@ -0,0 +1,10 @@
+package com.barath.app;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by barath.arivazhagan on 9/5/2017.
+ */
+@RestController
+public class FluxUserController {
+}
---
+++ src/main/java/com/barath/app/FluxUserService.java
@@ -0,0 +1,55 @@
+package com.barath.app;
+
+import com.barath.app.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.lang.invoke.MethodHandles;
+
+/**
+ * Created by barath.arivazhagan on 9/5/2017.
+ */
+
+@Service
+public class FluxUserService {
+
+
+    private static final Logger logger= LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+
+    private Flux<User> createSingleUser(){
+
+        return Flux.just(new User(1L,"barath",25));
+
+    }
+
+    private Flux<User> createFluxOfUsers(){
+
+        return getFluxOfUsers();
+    }
+
+
+    private Flux<String> getUserNames(){
+
+        return getFluxOfUsers().map(user -> user.getUserName());
+    }
+
+
+    public Flux<User> getFluxOfUsers(){
+        return Flux.just( new User(1L,"barath",25),
+                new User(2L,"DHONI",35),
+                new User(3L,"SACHIN",45),
+                new User(4L,"VIRAT",28),
+                new User(5L,"ROHIT",30),
+                new User(6L,"DHAWAN",28),
+                new User(7L,"KUMBLE",42),
+                new User(8L,"JOSHI",46),
+                new User(9L,"GAVASKAR",65),
+                new User(10L,"KAPILDEV",55),
+                new User(11L,"SRINATH",50),
+                new User(12L,"SACHIN",45));
+    }
+}
