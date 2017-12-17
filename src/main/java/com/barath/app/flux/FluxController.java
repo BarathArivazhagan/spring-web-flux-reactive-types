60833c5812208d5800fd65a55da58091e5162fef
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-12-17 16:36:54 +0530

    code commit for zip operation

--- src/main/java/com/barath/app/flux/FluxController.java
+++ src/main/java/com/barath/app/flux/FluxController.java
@@ -1,8 +1,6 @@
 package com.barath.app.flux;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 /**
@@ -51,4 +49,6 @@ public class FluxController {
         return fluxService.handleFluxWithError();
 
     }
+
+
 }
